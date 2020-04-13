b323fa7242788ed5b9c3b0cad8234c67a1157425
okezieobi <obiedereokezie@gmail.com>
2020-04-13 02:00:28 +0100

    Fixed eslint issue

--- src/backend/routes/logs.js
+++ src/backend/routes/logs.js
@@ -1,8 +1,7 @@
 export default (router) => {
   router.get('/on-covid-19/logs', (req, res) => {
     const logMessageObjects = res.locals.logData.logs.map(
-      ({ path, timestamp, processTime }) =>
-        `${timestamp}  ${path}  done in ${processTime} seconds`
+      ({ path, timestamp, processTime }) => `${timestamp}  ${path}  done in ${processTime} seconds`
     );
     const messages = logMessageObjects.join('\r\n');
     res.status(200).send(messages);
