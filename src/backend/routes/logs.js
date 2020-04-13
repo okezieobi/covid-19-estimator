5122a73cce5fb0d5c0c3738ead13dc27fb6ae4c1
okezieobi <obiedereokezie@gmail.com>
2020-04-13 01:30:57 +0100

    Added env for backend

--- src/backend/routes/logs.js
+++ src/backend/routes/logs.js
@@ -1,6 +1,9 @@
 export default (router) => {
   router.get('/on-covid-19/logs', (req, res) => {
-    const logMessageObjects = res.locals.logData.logs.map(({ path, timestamp, processTime }) => `${timestamp}  ${path}  done in ${processTime} sec(s)`);
+    const logMessageObjects = res.locals.logData.logs.map(
+      ({ path, timestamp, processTime }) =>
+        `${timestamp}  ${path}  done in ${processTime} seconds`
+    );
     const messages = logMessageObjects.join('\r\n');
     res.status(200).send(messages);
   });
