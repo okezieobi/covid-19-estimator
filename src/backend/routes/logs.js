6f82e4d2ef1339a1915c6fba4d27d12238bc5cfa
okezieobi <obiedereokezie@gmail.com>
2020-04-14 14:58:16 +0100

    35th fix updated logs router

--- src/backend/routes/logs.js
+++ src/backend/routes/logs.js
@@ -1,6 +1,5 @@
 export default (router) => {
   router.get('/on-covid-19/logs', (req, res) => {
-    res.locals.logData.logs.push();
     const logMessageObjects = res.locals.logData.logs.map(
       ({
         path, method, status, processTime
